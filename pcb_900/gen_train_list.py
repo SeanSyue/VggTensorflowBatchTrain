86bbfa0ea84d36b366214e93baca5619dbd74ed8
SeanSyue <Sean962081468@gmail.com>
2018-06-26 01:02:02 +0800

    add tf summary

--- pcb_900/gen_train_list.py
+++ pcb_900/gen_train_list.py
@@ -6,17 +6,19 @@ from pathlib import Path
 DATA_PATH = 'D:\WORKSPACE\DATASETS\Defects_Type1_6'
 N_SPLIT = 900
 
-train_chain = (list(item.glob('*'))[:N_SPLIT] for item in Path(DATA_PATH).glob('*/*'))
-train_list = [item.as_posix() for item in itertools.chain.from_iterable(train_chain)]
+if __name__ == '__main__':
+    train_chain = (list(item.glob('*'))[:N_SPLIT] for item in Path(DATA_PATH).glob('*/*'))
+    train_list = [item.as_posix() for item in itertools.chain.from_iterable(train_chain)]
 
-test_chain = (list(item.glob('*'))[N_SPLIT:] for item in Path(DATA_PATH).glob('*/*'))
-test_list = [item.as_posix() for item in itertools.chain.from_iterable(test_chain)]
+    test_chain = (list(item.glob('*'))[N_SPLIT:] for item in Path(DATA_PATH).glob('*/*'))
+    test_list = [item.as_posix() for item in itertools.chain.from_iterable(test_chain)]
 
-random.shuffle(train_list)
-random.shuffle(test_list)
+    random.shuffle(train_list)
+    random.shuffle(test_list)
 
-with open('train.lst', mode='w+', encoding='utf-8') as f_trn, open('test.lst', mode='w+', encoding='utf-8') as f_tst:
-    for item in train_list:
-        print(item, file=f_trn)
-    for item in test_list:
-        print(item, file=f_tst)
+    with open('train.lst', mode='w+', encoding='utf-8') as f_trn:
+        for item in train_list:
+            print(item, file=f_trn)
+    with open('test.lst', mode='w+', encoding='utf-8') as f_tst:
+        for item in test_list:
+            print(item, file=f_tst)
--- train_pcb.py
+++ train_pcb.py
@@ -4,57 +4,100 @@ Inspired by
 https://github.com/machrisaa/tensorflow-vgg/issues/11#issuecomment-262748616
 https://github.com/machrisaa/tensorflow-vgg/issues/11#issuecomment-262793514
 """
+from os.path import join as join_path
+from os.path import isdir
+from os import makedirs
 import numpy as np
 import tensorflow as tf
 from Vgg.vgg19_trainable import Vgg19Resizable
 from Vgg.utils import load_image, gen_batch, get_label
 
-WEIGHTS_FILE = './weights/vgg19.npy'
-TRAIN_FILE = 'pcb_900/train.lst'
+TASK = 'conf_1'
 N_BATCH = 64
 N_CLASS = 2
 L_RATE = 1e-4
 MAX_ITER = 10000
 
-# Prepare training data
-train_list = [item.strip() for item in open(TRAIN_FILE, encoding='utf-8').readlines()]
-gen = gen_batch(train_list, N_BATCH)
+PARENT = './pcb_900'
+WEIGHTS_FILE = './weights/vgg19.npy'
+TRAIN_FILE = join_path(PARENT, 'train.lst')
+SAVED_WEIGHTS = './pcb_900/{}/weights'.format(TASK)
+SAVED_SUMMARY = './pcb_900/{}/summary'.format(TASK)
 
-# Define default graph
-graph = tf.Graph()
-with graph.as_default():
-    images = tf.placeholder(tf.float32, [N_BATCH, 224, 224, 3])
-    labels = tf.placeholder(tf.float32, [N_BATCH, N_CLASS])
-    train_mode = tf.placeholder(tf.bool)
+if __name__ == '__main__':
 
-    vgg = Vgg19Resizable(WEIGHTS_FILE)
-    vgg.build(images, train_mode)
+    if not isdir(SAVED_SUMMARY):
+        makedirs(SAVED_SUMMARY)
 
-    cost = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=vgg.prob)
-    train = tf.train.AdamOptimizer(L_RATE).minimize(cost)
-    correct_prediction = tf.equal(tf.argmax(vgg.prob, 1), tf.argmax(labels, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    if not isdir(SAVED_WEIGHTS):
+        makedirs(SAVED_WEIGHTS)
 
-# Train loop
-with tf.Session(graph=graph) as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(MAX_ITER):
-        cur_imgs = next(gen)
-        batch_img_raw = list(map(load_image, cur_imgs))
-        batch_img_arr = np.array(batch_img_raw).reshape((N_BATCH, 224, 224, 3))
-        batch_lab_raw = list(map(get_label, cur_imgs))
-        batch_lab_arr = np.array(batch_lab_raw)
+    # Prepare training data
+    train_list = [item.strip() for item in open(TRAIN_FILE, encoding='utf-8').readlines()]
+    gen = gen_batch(train_list, N_BATCH)
 
-        feed_dict = {
-            images: batch_img_arr,
-            labels: batch_lab_arr,
-            train_mode: True
-        }
+    # Define default graph
+    graph = tf.Graph()
+    with graph.as_default():
+        images = tf.placeholder(tf.float32, [N_BATCH, 224, 224, 3])
+        labels = tf.placeholder(tf.float32, [N_BATCH, N_CLASS])
+        train_mode = tf.placeholder(tf.bool)
 
-        _, cost_val, accu_val = sess.run([train, cost, accuracy], feed_dict=feed_dict)
+        # initialize network
+        vgg = Vgg19Resizable(WEIGHTS_FILE)
+        vgg.build(images, train_mode)
 
-        info = 'Step: {} | Loss: {:0.5f} | Accuracy: {:0.5f} | L_rate: {}'.format(i, cost_val, accu_val, L_RATE)
-        print(info)
+        with tf.name_scope('cost'):
+            with tf.name_scope('cost_fn'):
+                cost = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=vgg.prob)
+            with tf.name_scope('cost'):
+                cost_avg = tf.reduce_mean(tf.cast(cost, tf.float32))
+        tf.summary.scalar('cost_avg', cost_avg)
 
-        if i % 500 == 0:
-            vgg.save_npy(sess, 'pcb_900/weights')
+        with tf.name_scope('train'):
+            train_step = tf.train.AdamOptimizer(L_RATE).minimize(cost)
+
+        with tf.name_scope('accuracy'):
+            with tf.name_scope('correct_prediction'):
+                correct_prediction = tf.equal(tf.argmax(vgg.prob, 1), tf.argmax(labels, 1))
+            with tf.name_scope('accuracy'):
+                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
+
+    # define default session
+    with tf.Session(graph=graph) as sess:
+        sess.run(tf.global_variables_initializer())
+
+        merged = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter(join_path(SAVED_SUMMARY, 'train'), graph=sess.graph)
+        test_writer = tf.summary.FileWriter(join_path(SAVED_SUMMARY, 'test'), graph=sess.graph)
+
+        # Train loop
+        for i in range(MAX_ITER):
+            # get train batches
+            cur_batch = next(gen)
+            batch_img_raw = list(map(load_image, cur_batch))
+            batch_img_arr = np.array(batch_img_raw).reshape((N_BATCH, 224, 224, 3))
+            batch_lab_raw = list(map(get_label, cur_batch))
+            batch_lab_arr = np.array(batch_lab_raw)
+
+            feed_dict = {
+                images: batch_img_arr,
+                labels: batch_lab_arr,
+                train_mode: True
+            }
+
+            if i % 10 == 0:
+                # Record summaries and test-set accuracy
+                accu_val, summary = sess.run([accuracy, merged], feed_dict=feed_dict)
+                test_writer.add_summary(summary, i)
+                print('Step: {} | Accuracy: {:0.5f}'.format(i, accu_val))
+            else:
+                # Record train set summaries, and train
+                cost_val, summary, _ = sess.run([cost_avg, merged, train_step], feed_dict=feed_dict)
+                train_writer.add_summary(summary, i)
+                print('Step: {} | Loss: {:0.5} | L_rate: {}'.format(i, cost_val, L_RATE))
+
+            # save weights
+            if i % 500 == 0:
+                vgg.save_npy(sess, join_path(SAVED_WEIGHTS, 'step_{}'.format(i)))
